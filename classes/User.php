edfc0c1c4a3c64589be8fa9845105b85cfe13367
Nugget <cdl.0404@gmail.com>
2022-03-08 12:09:48 +0100

    Added basic user class

---
+++ classes/User.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+class User
+{
+    private string $username;
+
+    public function __construct(string $username)
+    {
+        $this->username = $username;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+}
