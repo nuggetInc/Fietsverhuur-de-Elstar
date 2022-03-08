fba6c0134a3b9f7d8707689fa5c90ea15ce29294
Nugget <cdl.0404@gmail.com>
2022-03-08 14:30:18 +0100

    Started login page

--- classes/Database.php
+++ classes/Database.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require_once("Employee.php");
+
 class Database
 {
     public static function getPDO(): PDO
@@ -9,5 +11,50 @@ class Database
         static $pdo = new PDO("mysql:host=localhost;dbname=fietsverhuur_de_elstar", "root", "");
 
         return $pdo;
+    }
+
+    public static function hasEmployeeName(string $username): bool
+    {
+        $params = array(":name" => $username);
+        $sth = self::getPDO()->prepare("SELECT 1 FROM `employee` WHERE `name` = :name;");
+        $sth->execute($params);
+
+        return $sth->rowCount() > 0;
+    }
+
+    public static function getEmployee(string $username): ?Employee
+    {
+        $params = array(":name" => $username);
+        $sth = self::getPDO()->prepare("SELECT `name`, `permission` FROM `employee` WHERE `name` = :name;");
+        $sth->execute($params);
+
+        if ($row = $sth->fetch())
+        {
+            $name = $row["name"];
+            $permission = Permission::from($row["permission"]);
+            return new Employee($name, $permission);
+        }
+
+        return null;
+    }
+
+    public static function getEmployeeHash(Employee $user): ?string
+    {
+        $params = array(":name" => $user->GetName());
+        $sth = self::getPDO()->prepare("SELECT `hash` FROM `employee` WHERE `name` = :name;");
+        $sth->execute($params);
+
+        return $sth->fetchColumn(0);
+    }
+
+    public static function registerEmployee(string $name, string $password, Permission $permission): Employee
+    {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $params = array(":name" => $name, ":hash" => $hash, ":permission" => $permission->value);
+        $sth = self::getPDO()->prepare("INSERT INTO `employee` (`name`, `hash`, `permission`) VALUES (:name, :hash, :permission);");
+        $sth->execute($params);
+
+        return new Employee($name, $permission);
     }
 }
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+require_once("classes/Employee.php");
+
+session_start();
+
+if (isset($_SESSION["user"]))
+{
+    header("Location: home/");
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="login.php" method="POST">
+        <label>Username
+            <input type="text" name="name" placeholder="Username" maxlength="24" required />
+        </label>
+        <label>Password
+            <input type="password" name="password" placeholder="Password" required />
+        </label>
+        <input class="submit" type="submit" value="Login" />
+    </form>
+</body>
+
+</html>
