27a898db20186bf4c23b99a2c4a8bba203b09311
DustVeer <dveerdonk@gmail.com>
2022-03-15 08:46:44 +0100

    Font

--- pages/reserve.php
+++ pages/reserve.php
@@ -21,23 +21,23 @@ if (!isset($_SESSION["user"]))
         <th>Zondag</th>
     </tr>
     <?php
-    $getdate_wday = (getdate()["wday"] == 0) ? 7 : getdate()["wday"] - 1;
+    $getdate_wday = (getdate()["wday"] == 0) ? 6 : getdate()["wday"] - 1;
     $date = date("d-m-Y", strtotime("-" . $getdate_wday .  " days"));
-        for($j = 0; $j < 20; $j++) 
+    for($j = 0; $j < 20; $j++) 
+    {
+        echo "<tr>";
+        for($i = 0; $i < 7; $i++) 
         {
-            echo "<tr>";
-            for($i = 0; $i < 7; $i++) 
-            {
-                $color = ($date == date("d-m-Y")) ? "green" : ((getdate(strtotime($date))["wday"] == 6 ||getdate(strtotime($date))["wday"]== 0) ? "gray" : "lightgray");
-                echo "
-                <td style='background-color: {$color}'>
-                    <p class='table-text'>{$date}</p>
-                    <p class='table-text'>25/100</p>
-                    <button><a>Reserveer</a></button>
-                </td>";
-                $date = date("d-m-Y", strtotime($date .  "+1 days"));
-            }
-            echo "</tr>";
+            $color = ($date == date("d-m-Y")) ? "green" : ((getdate(strtotime($date))["wday"] == 6 ||getdate(strtotime($date))["wday"]== 0) ? "gray" : "lightgray");
+            echo "
+            <td style='background-color: {$color}'>
+                <p class='table-text'>{$date}</p>
+                <p class='table-text'>25/100</p>
+                <button><a>Reserveer</a></button>
+            </td>";
+            $date = date("d-m-Y", strtotime($date .  "+1 days"));
         }
+        echo "</tr>";
+    }
     ?>
 </table>
