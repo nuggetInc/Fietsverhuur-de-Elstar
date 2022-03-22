53f21fff2ea05cfe560a7f7b3bba30fa2522aedc
DustVeer <dveerdonk@gmail.com>
2022-03-22 13:59:32 +0100

    Create_reserver new way

--- pages/reserve/create_reserve.php
+++ pages/reserve/create_reserve.php
@@ -16,21 +16,30 @@ if (isset($_POST["customer"]) && is_numeric($_POST["customer"]))
 
 <form action="" method="POST">
     <header>Nieuwe reservering</header> 
-    <label>Klant id</label>
-    
+    <?php if(isset($_POST["customer"])) : ?>
+        <label>Klant</label>
+    <?php else: ?>
+        <label>Klant id</label>
+    <?php endif ?>
     <input  type="text" name="customer" value="<?= $displayCustomer ?>" placeholder="Klant id">
-</form>
-<form>
-    <label>Datum vanaf</label>
+
+    <?php if(isset($_POST["customer"])) : ?>
+        <label>Aantal personen/fietsen</label>
+        <input type="number" name="numberOfPeople" placeholder="Aantal personen/fietsen ">
+    <?php endif ?>
+
+    
+    <!-- <label>Datum vanaf</label>
     <input type="date" name="dateForm">   
     <label>Datum tot</label>
     <input type="date" name="dateTo">
-    <label>Aantal personen</label>
-    <input type="number" name="numberOfPeople" placeholder="Aantal personen">
+    
+    <label>Opmerking</label>
+    <textarea></textarea>
     <div class="inline">
         <label>Kinderzitje:  <input type="checkbox" name="child_seat"></label>
     </div>
-    
+     -->
         
     <input type="submit">
 </form>
