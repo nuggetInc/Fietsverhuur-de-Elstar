0da39b07560b68a65533ebbd7cd889c8d2c8d5bd
Nugget <cdl.0404@gmail.com>
2022-03-22 13:19:43 +0100

    *framenumber

--- classes/Bike.php
+++ classes/Bike.php
@@ -6,18 +6,18 @@ require_once("Database.php");
 
 class Bike
 {
-    private string $framenummer;
+    private string $framenumber;
     private string $comment;
 
-    public function __construct(string $framenummer, string $comment)
+    public function __construct(string $framenumber, string $comment)
     {
-        $this->framenummer = $framenummer;
+        $this->framenumber = $framenumber;
         $this->comment = $comment;
     }
 
-    public function getFramenummer(): string
+    public function getFramenumber(): string
     {
-        return $this->framenummer;
+        return $this->framenumber;
     }
 
     public function getComment(): string
@@ -27,21 +27,21 @@ class Bike
 
     public static function getTotalBikeCount(): int
     {
-        $sth = Database::getPDO()->prepare("SELECT 1 FROM `bike` WHERE `framenummer` != '';");
+        $sth = Database::getPDO()->prepare("SELECT 1 FROM `bike` WHERE `framenumber` != '';");
         $sth->execute();
         return $sth->rowCount();
     }
 
     public static function getBikes(): array
     {
-        $sth = Database::getPDO()->prepare("SELECT `framenummer`, `comment` FROM `bike` WHERE `framenummer` != '';");
+        $sth = Database::getPDO()->prepare("SELECT `framenumber`, `comment` FROM `bike` WHERE `framenumber` != '';");
         $sth->execute();
 
         $bikes = array();
 
         while ($row = $sth->fetch())
         {
-            $bikes[] = new Bike($row["framenummer"], $row["comment"]);
+            $bikes[] = new Bike($row["framenumber"], $row["comment"]);
         }
 
         return $bikes;
