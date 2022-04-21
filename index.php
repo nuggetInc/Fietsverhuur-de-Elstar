af2dbcd7dff43010b2ddb93f6f18241860e68118
Nugget <cdl.0404@gmail.com>
2022-04-21 13:58:39 +0200

    formatting

--- index.php
+++ index.php
@@ -29,11 +29,7 @@ const ROOT = "/Fietsverhuur-de-Elstar/";
 </head>
 
 <body>
-    <?php
-
-    $uri = $_SERVER["REQUEST_URI"];
-
-    ?>
+    <?php $uri = $_SERVER["REQUEST_URI"]; ?>
     <?php if (isset($_SESSION["user"])) : ?>
         <?php $children = Page::getRootChildren(); ?>
         <header id="header">
